4cf73d8bbc217a2be2ccec23480609a0ea06a680
Anna Sedakova <tuzik198@mail.ru>
2018-04-27 12:43:17 +0300

    Summary site portfolio

--- js/main.js
+++ js/main.js
@@ -3,10 +3,8 @@ $(document).ready(function() {
 
 //slide to id - плавная прокрутка
 
-$("nav a,a[href='#top'],a[rel='m_PageScroll2id'],a.PageScroll2id").mPageScroll2id({
-	highlightSelector:"nav a"
-});
-
-
+	$("nav a,a[href='#top'],a[rel='m_PageScroll2id'],a.PageScroll2id,a.mouse_scroll ").mPageScroll2id({
+	    highlightSelector:"nav a"
+	});
 
 });
