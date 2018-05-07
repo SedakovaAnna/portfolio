4951de1b981c9142ea0693af20db353fedabb7ff
Anna Sedakova <tuzik198@mail.ru>
2018-05-07 10:55:45 +0300

    Contacts site portfolio

--- js/main.js
+++ js/main.js
@@ -22,12 +22,59 @@ $('#portfolio-projects').mixItUp();
     });
 // End of FancyBox - galery
 
-
+// фильтр
 var filterButton = $('.filter-block__button');
 	var filterButtonActive = 'filter-block__button--active';
 
 	filterButton.on('click', function(){
 		$(this).addClass(filterButtonActive).parent().siblings().children().removeClass(filterButtonActive);
 });
+
+
+// jQuery валидация
+$("#contact-form").validate({
+	rules: {
+		name: { required: true },
+		email: { required: true, email: true },
+		// phone: { required: true },
+		// skype: { required: true },
+		message: { required: true }
+	},
+
+	messages: {
+		name: "Пожалуйста, введите имя",
+		email: {
+			required: "Пожалуйста, введите свой email",
+			email: "Email адрес должен быть в формате name@domain.com . Возможно вы ввели email с ошибкой."
+		},
+		message: "Пожалуйста, введите текст сообщения"
+	},
+
+		submitHandler: function(form) {
+		  ajaxFormSubmit();
+		}
+})
+
+	// Функция AJAX запрса на сервер
+	function ajaxFormSubmit(){
+		var string = $("#contact-form").serialize(); // Соханяем данные введенные в форму в строку. 
+
+		// Формируем ajax запрос
+		$.ajax({
+			type: "POST", // Тип запроса - POST
+			url: "php/mail.php", // Куда отправляем запрос
+			data: string, // Какие даные отправляем, в данном случае отправляем переменную string
+			
+			// Функция если все прошло успешно
+			success: function(html){
+				$("#contact-form").slideUp(800);
+				$('#answer').html(html);
+			}
+		});
+
+		// Чтобы по Submit больше ничего не выполнялось - делаем возврат false чтобы прервать цепчку срабатывания остальных функций
+		return false; 
+	}
+
 
 });
--- js/navigation.js
+++ js/navigation.js
@@ -1,40 +1,75 @@
-//мобильная навигация
+//мобильная навигация иконка
 
-	var navToggleButton = $('.navigation__toggle');
-	var navToggleIcon = $('.navigation__toggle .fa');
-	var navList = $('.navigation__list');
-	var navLink = $('.navigation__link');
+	// var navToggleButton = $('.navigation__toggle');
+	// var navToggleIcon = $('.navigation__toggle .fa');
+	// var navList = $('.navigation__list');
+	// var navLink = $('.navigation__link');
 
-	var navListOpen = 'navigation__list--open';
-	var iconNav = 'fa-bars';
-	var iconClose = 'fa-times';
+	// var navListOpen = 'navigation__list--open';
+	// var iconNav = 'fa-bars';
+	// var iconClose = 'fa-times';
 
-	navToggleButton.on('click', function(e){
-		e.preventDefault();
-		navList.toggleClass(navListOpen);
+	// navToggleButton.on('click', function(e){
+	// 	e.preventDefault();
+	// 	navList.toggleClass(navListOpen);
 
-		if( navToggleIcon.hasClass(iconNav) ){
-			navToggleIcon.removeClass(iconNav);
-			navToggleIcon.addClass(iconClose);
+	// 	if( navToggleIcon.hasClass(iconNav) ){
+	// 		navToggleIcon.removeClass(iconNav);
+	// 		navToggleIcon.addClass(iconClose);
+	// 	} else {
+	// 		navToggleIcon.removeClass(iconClose);
+	// 		navToggleIcon.addClass(iconNav);
+	// 	}
+
+	// });
+
+	// navLink.on('click', function(){
+
+	// 	if ( navList.hasClass(navListOpen) ) {
+	// 		if( navToggleIcon.hasClass(iconNav) ){
+	// 			navToggleIcon.removeClass(iconNav);
+	// 			navToggleIcon.addClass(iconClose);
+	// 		} else {
+	// 			navToggleIcon.removeClass(iconClose);
+	// 			navToggleIcon.addClass(iconNav);
+	// 		}
+	// 	}
+
+	// 	navList.removeClass(navListOpen);
+
+	// });
+
+//мобильная навигация кнопка
+
+var navToggleButton = $('#navigation__button');
+var navToggleIcon = $('.navigation__toggle .fa');
+var navList = $('.navigation__list');
+var navLink = $('.navigation__link');
+var navListOpen = 'navigation__list--open';
+//клик по иконке
+navToggleButton.on('click', function(e){
+	e.preventDefault();
+	navList.toggleClass(navListOpen);
+
+	if ( navToggleButton.hasClass("active")) {
+		navToggleButton.removeClass("active");
+	} else {
+		navToggleButton.addClass("active");
+	}
+
+});
+//клик на ссылку
+navLink.on('click', function(){
+
+	if ( navList.hasClass(navListOpen) ) {
+
+		if ( navToggleButton.hasClass("active")) {
+			navToggleButton.removeClass("active");
 		} else {
-			navToggleIcon.removeClass(iconClose);
-			navToggleIcon.addClass(iconNav);
+			navToggleButton.addClass("active");
 		}
+	}
 
-	});
+	navList.removeClass(navListOpen);
 
-	navLink.on('click', function(){
-
-		if ( navList.hasClass(navListOpen) ) {
-			if( navToggleIcon.hasClass(iconNav) ){
-				navToggleIcon.removeClass(iconNav);
-				navToggleIcon.addClass(iconClose);
-			} else {
-				navToggleIcon.removeClass(iconClose);
-				navToggleIcon.addClass(iconNav);
-			}
-		}
-		
-		navList.removeClass(navListOpen);
-
-	});
+});
